126eab734df2b24b44189272dd63510b5f0c99c7
ibrahimhamza01 <ibrahim.hamza01@gmail.com>
2025-12-03 22:48:09 -0500

    Add Q1.r

---
+++ Scirpts/Q1.R
@@ -0,0 +1,16 @@
+brfss = readRDS("../Data/Processed/brfss_2018_2023.rds")
+
+brfss_postcovid <- subset(brfss, interview_year >= 2020)
+
+brfss_postcovid$obese <- ifelse(brfss_postcovid$bmi >= 30, 1, 0)
+brfss_postcovid$obese <- factor(brfss_postcovid$obese, labels = c("No","Yes"))
+
+model1 <- glm(
+  obese ~ age + sex + race_ethnicity + income_category +
+    any_exercise_last_month + binge_drinking +
+    max_drinks_30day + interview_year,
+  data = brfss_postcovid,
+  family = binomial
+)
+
+summary(model1)
