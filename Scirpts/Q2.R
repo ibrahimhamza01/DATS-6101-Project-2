b4775fb48a99b174ff403811494853326aed11cd
dhowell0712 <dhowell07@email.gwu.edu>
2025-12-09 02:44:42 -0500

    Add files via upload

--- Scirpts/Q2.R
+++ Scirpts/Q2.R
@@ -6,16 +6,19 @@ library(tidyr)
 library(ezids)
 library(car)
 library(pROC)
+library(pscl)
+library(corrplot)
 
 setwd("C:/Users/dhowell07/Documents/DATS6101/R/Research Project/Project2")
 
 brfss = read.csv("brfss_2018_2023.csv", header=T)
 
+
 ##2. Physical Activity and BMI: 
 ##To what extent can physical activity level predict an individual’s BMI, 
 ##controlling for demographic covariates and survey year?
 
-brfss_1 = brfss[,c(1:8,12:16,24)]
+brfss_1 = brfss[,c(1:8,12:16,19:22,24)]
 
 ##Check class of variables
 str(brfss_1)
@@ -30,7 +33,7 @@ brfss_1$bmi_category <- factor(brfss_1$bmi_category, order=T,levels = c("Underwe
 brfss_1$overweight_or_obese = as.factor(brfss_1$overweight_or_obese)
 brfss_1$overweight_or_obese <- factor(brfss_1$overweight_or_obese, order=T,levels = c("Yes","No"))
 brfss_1$any_exercise_last_month = as.factor(brfss_1$any_exercise_last_month)
-brfss_1$any_exercise_last_month <- factor(brfss_1$any_exercise_last_month, order=T,levels = c("Yes","No"))
+brfss_1$any_exercise_last_month <- factor(brfss_1$any_exercise_last_month, order=T,levels = c("No","Yes"))
 brfss_1$sex = as.factor(brfss_1$sex)
 brfss_1$sex <- factor(brfss_1$sex, order=F)
 brfss_1$age_group = as.factor(brfss_1$age_group)
@@ -39,10 +42,85 @@ brfss_1$race_ethnicity = as.factor(brfss_1$race_ethnicity)
 brfss_1$race_ethnicity <- factor(brfss_1$race_ethnicity, order=F)
 brfss_1$education_level = as.factor(brfss_1$education_level)
 brfss_1$education_level <- factor(brfss_1$education_level, order=T,levels = c("No school/K","Grades 1-8","Grades 9-11","Grade 12/GED","College 1-3 years","College 4+ years"))
+brfss_1$diabetes_status = as.factor(brfss_1$diabetes_status)
+brfss_1$diabetes_status <- factor(brfss_1$diabetes_status, order=T,levels = c("No","Yes","Pre-diabetes","Yes, only pregnancy"))
+brfss_1$coronary_hd_history = as.factor(brfss_1$coronary_hd_history)
+brfss_1$coronary_hd_history <- factor(brfss_1$coronary_hd_history, order=T,levels = c("No","Yes"))
+brfss_1$stroke_history = as.factor(brfss_1$stroke_history)
+brfss_1$stroke_history <- factor(brfss_1$stroke_history, order=T,levels = c("No","Yes"))
 brfss_1$self_reported_health = as.factor(brfss_1$self_reported_health)
 brfss_1$self_reported_health <- factor(brfss_1$self_reported_health, order=T,levels = c("Good or Better","Fair or Poor"))
 
 str(brfss_1)
+
+brfss_1 <- brfss_1 %>%
+  mutate(obese = ifelse(bmi >= 30, 1, 0))
+brfss_1 <- brfss_1 %>%
+  mutate(exercise = ifelse(any_exercise_last_month == "No", 0, 1))
+brfss_1 <- brfss_1 %>%
+  mutate(diabetes = ifelse(diabetes_status == "No", 0, 1))
+brfss_1 <- brfss_1 %>%
+  mutate(heart_attack = ifelse(heart_attack_history == "No", 0, 1))
+brfss_1 <- brfss_1 %>%
+  mutate(coronary = ifelse(coronary_hd_history == "No", 0, 1))
+brfss_1 <- brfss_1 %>%
+  mutate(stroke = ifelse(stroke_history == "No", 0, 1))
+brfss_1 <- brfss_1 %>%
+  mutate(self_health = ifelse(self_reported_health == "Fair or Poor", 0, 1))
+
+str(brfss_1)
+
+# Spearman Correlation Matrix
+brfss_nums = brfss_1[,c(5,10,19:25)]
+str(brfss_nums)
+spearman_corr <- cor(brfss_nums, use = "complete.obs", method = "spearman")
+print(spearman_corr)
+corrplot(spearman_corr, method = "color", type = "upper", tl.cex = 0.8)
+
+brfss_1$obese = as.factor(brfss_1$obese)
+brfss_1$obese <- factor(brfss_1$obese, order=T,levels = c("0","1"))
+brfss_1$exercise = as.factor(brfss_1$exercise)
+brfss_1$exercise <- factor(brfss_1$exercise, order=T,levels = c("0","1"))
+brfss_1$diabetes = as.factor(brfss_1$diabetes)
+brfss_1$diabetes <- factor(brfss_1$diabetes, order=T,levels = c("0","1"))
+brfss_1$heart_attack = as.factor(brfss_1$heart_attack)
+brfss_1$heart_attack <- factor(brfss_1$heart_attack, order=T,levels = c("0","1"))
+brfss_1$coronary = as.factor(brfss_1$coronary)
+brfss_1$coronary <- factor(brfss_1$coronary, order=T,levels = c("0","1"))
+brfss_1$stroke = as.factor(brfss_1$stroke)
+brfss_1$stroke <- factor(brfss_1$stroke, order=T,levels = c("0","1"))
+brfss_1$self_health = as.factor(brfss_1$self_health)
+brfss_1$self_health <- factor(brfss_1$self_health, order=T,levels = c("0","1"))
+
+str(brfss_1)
+
+##Fitting a line to between BMI and Exercise
+fit1 <- lm(bmi ~ exercise, data = brfss_1)
+summary(fit1)
+xkabledply(fit1, title = paste("Model (factor):", format(formula(fit1)) ) )
+xkablevif(fit1)
+plot(fit1)
+
+##Fitting a line to between BMI, Exercise, and Age
+fit2 <- lm(bmi ~ exercise+age+self_health, data = brfss_1)
+summary(fit2)
+xkabledply(fit2, title = paste("Model (num):", format(formula(fit2)) ) )
+xkablevif(fit2)
+plot(fit2)
+
+##Fitting a line to between BMI, Exercise, and Age
+fit3 <- lm(bmi ~ exercise+age+self_reported_health+diabetes+stroke, data = brfss_1)
+summary(fit3)
+#xkabledply(fit3, title = paste("Model (num):", format(formula(fit3)) ) )
+xkablevif(fit3)
+plot(fit3)
+
+##Interaction Fit
+mixfit1 = lm(bmi~exercise+age + exercise:self_health, data = brfss_1)
+summary(mixfit1)
+
+mixfit2 <- lm(bmi ~ exercise+age+self_reported_health+diabetes*stroke, data = brfss_1)
+summary(mixfit2)
 
 ## Effects of Physical Activity on BMI Category
 
@@ -81,8 +159,6 @@ bmi_selfhealthtable2
 chisqselfhealth2 = chisq.test(bmi_selfhealthtable2)
 chisqselfhealth2
 
-
-
 ## Effects of Physical Activity on BMI
 
 ### Two-way T-test
@@ -95,6 +171,9 @@ t.test(brfss_1$bmi ~ brfss_1$any_exercise_last_month)
 
 mean_bmi=aggregate(bmi~any_exercise_last_month,data=brfss_1,FUN = mean)
 mean_bmi
+
+
+
 
 ## Logistic Regression Model for physical activity in the last month (bmi)
 activityLogit <- glm(any_exercise_last_month ~ bmi , data = brfss_1, family = "binomial")
@@ -112,7 +191,7 @@ plot(h)
 
 
 ## Logistic Regression Model for physical activity in the last month (bmi & age)
-activityLogit2 <- glm(any_exercise_last_month ~ bmi + age, data = brfss_1, family = "binomial")
+activityLogit2 <- glm(any_exercise_last_month ~ bmi + age + self_reported_health, data = brfss_1, family = "binomial")
 summary(activityLogit2)
 
 #### Receiver-Operator-Characteristic (ROC) curve and Area-Under-Curve (AUC)
@@ -126,7 +205,7 @@ auc(i) # area-under-curve prefer 0.8 or higher.
 plot(i)
 
 ## Logistic Regression Model for bmi overweight/obese in the last month (bmi)
-activityLogit3 <- glm(overweight_or_obese ~ any_exercise_last_month + age, data = brfss_1, family = "binomial")
+activityLogit3 <- glm(obese ~ exercise + age +self_health, data = brfss_1, family = "binomial")
 summary(activityLogit3)
 
 #### Receiver-Operator-Characteristic (ROC) curve and Area-Under-Curve (AUC)
@@ -135,6 +214,35 @@ summary(activityLogit3)
 
 prob3=predict(activityLogit3, type = "response" )
 brfss_1$prob3=prob3
-j <- roc(overweight_or_obese~prob3, data=brfss_1)
+j <- roc(obese~prob3, data=brfss_1)
 auc(j) # area-under-curve prefer 0.8 or higher.
 plot(j)
+
+
+##McFadden for model 2 (bmi & age)
+library(pscl) # use pR2( ) function to calculate McFadden statistics for model eval
+activityLogit3pr2 = pR2(activityLogit3)
+activityLogit3pr2
+unloadPkg("pscl")
+
+###
+
+## Logistic Regression Model for bmi overweight/obese in the last month (bmi)
+activityLogit4 <- glm(obese ~ exercise + age+self_health+diabetes+stroke, data = brfss_1, family = "binomial")
+summary(activityLogit4)
+
+#### Receiver-Operator-Characteristic (ROC) curve and Area-Under-Curve (AUC)
+
+## Receiver-Operator-Characteristic (ROC) curve and Area-Under-Curve (AUC) measures the true positive rate (or sensitivity) against the false positive rate (or specificity). The area-under-curve is always between 0.5 and 1. Values higher than 0.8 is considered good model fit.  
+
+prob4=predict(activityLogit4, type = "response" )
+brfss_1$prob4=prob4
+j <- roc(overweight_or_obese~prob4, data=brfss_1)
+auc(j) # area-under-curve prefer 0.8 or higher.
+plot(j)
+
+##McFadden for model 2 (bmi & age)
+library(pscl) # use pR2( ) function to calculate McFadden statistics for model eval
+activityLogit4pr2 = pR2(activityLogit4)
+activityLogit4pr2
+unloadPkg("pscl")
